34d0b6e382bf0230be179b1f1c5ed38ea5491d92
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-06-05 02:26:58 +0100

    Added sanity schemas to be used as backend for this project

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,1 +1,11 @@
-export const schemaTypes = []
+import user from './user'
+import post from './post'
+import comment from './comment'
+import postedBy from './postedBy'
+
+export const schemaTypes = [
+    user,
+    post,
+    comment,
+    postedBy
+]
